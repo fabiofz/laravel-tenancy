90757c88b444c4c793d0d3a870ccf0d2bcc1455d
Fabio Franzoni <fabio.franzoni@uello.com.br>
2022-09-29 10:12:44 -0300

    Mudando para api

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/tenancy', function () {
-    return "We try some tenancy!";
-});
